7189b901c64d2bad2727e69018ffa01ba060883b
Heming0425 <dengdeemo@outlook.com>
2018-11-26 11:15:33 +0800

    version 0.2
    *完善了登陆注册，增添了session验证保持登陆状态
    *增加了基本的文章上传功能和对应的数据库模型
    *增加了个人信息展示页面

--- app/models.py
+++ app/models.py
@@ -1,17 +1,17 @@
 from app import db
 from datetime import datetime
 
-class User_log(db.Model):
 
-    # 表名
-    __tablename__ = 'user_log'
+class User_info(db.Model): #用户信息表
 
-    id = db.Column(db.Integer,primary_key=True)
-    uid = db.Column(db.String(225),unique=True)
+    __tablename__ = 'user_info'
+
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.String(225), unique=True)
     password = db.Column(db.String(225))
     name = db.Column(db.String(225))
     email = db.Column(db.String(225))
-    register_login = db.Column(db.DateTime()) #注册时间
+    register_login = db.Column(db.DateTime())  # 注册时间
 
     def __init__(self, uid, password, name, email, register_login):
         self.uid = uid
@@ -19,6 +19,28 @@ class User_log(db.Model):
         self.name = name
         self.email = email
         self.register_login = register_login
-    
+
     def __repr__(self):
         return "<model uid '{}'>".format(self.uid)
+
+
+class Blog_info(db.Model): #博客信息表
+
+    __tablename__ = 'blog_info'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(225))
+    text = db.Column(db.String(225))
+    imag = db.Column(db.String(225))
+    audio = db.Column(db.String(225))
+    blog_type = db.Column(db.Integer)
+    upload_time = db.Column(db.DateTime())
+    user_id = db.Column(db.String(225), db.ForeignKey('user_info.uid'))
+
+    def __init__(self, title, text, imag, audio, blog_type,upload_time, user_id):
+        self.title = title
+        self.text = text
+        self.imag = imag
+        self.audio = audio
+        self.blog_type = blog_type
+        self.user_id = user_id
+        self.upload_time = upload_time
--- app/routers.py
+++ app/routers.py
@@ -8,44 +8,63 @@ from werkzeug.utils import secure_filename
 # 导入数据库
 from app import app
 from app import db
-from .models import User_log
+from .models import User_info, Blog_info
 
 '''
 登陆注册模块
 '''
 
 
-@app.route('/login', methods=['GET','POST'])  # 登陆
+@app.route('/login', methods=['GET', 'POST'])  # 登陆
 def login():
     if request.method == 'POST':
         uid = request.form.get('uid')
         password = request.form.get('password')
-        u_data = User_log.query.filter_by(uid=uid).first()
-        if check_password_hash(u_data.password,password):
-            return '登陆成功'
-        else:
-            return '登录失败'
+        u_data = User_info.query.filter_by(uid=uid).first()
+        try:
+            rp = u_data.password
+            if check_password_hash(rp, password):
+                session['uid'] = uid
+                return redirect('index')
+            else:
+                flash('密码错误')
+                return render_template('login.html', flash=flash)
+        except:
+            flash('用户不存在')
+            return render_template('login.html', flash=flash)
     else:
         return render_template('login.html')
 
 
-@app.route('/registered', methods=['GET','POST'])  # 注册
+@app.route('/registered', methods=['GET', 'POST'])  # 注册
 def registered():
     if request.method == 'POST':
-        uid = request.form.get('uid')
+
+        uid = request.form.get('uid')  # 验证用户名
+        try:
+            if User_info.query.filter_by(uid=uid).first().uid:
+                flash('该用户名存在')
+                return render_template('registered.html', flash=flash)
+        except:
+            pass
+
         password = request.form.get('password')
-        # rpassword = request.form.get('rpassword')
+        rpassword = request.form.get('rpassword')
+
+        if password != rpassword:  # 验证密码
+            flash('两次密码不一致')
+            render_template('registered.html', flash=flash)
         name = request.form.get('name')
         email = request.form.get('email')
 
         password = generate_password_hash(password)  # 密码加密
 
-        user = User_log(
+        user = User_info(
             uid=uid,
             password=password,
             name=name,
             email=email,
-            register_login = datetime.now()
+            register_login=datetime.now()
         )
 
         db.session.add(user)  # 向数据库添加用户信息
@@ -65,6 +84,13 @@ def registered():
 @app.route('/index')  # 主页
 def index():
 
+    # 验证session
+    try:
+        uid = session['uid']
+    except:
+        flash('请先登陆')
+        return render_template('login.html', flash=flash)
+
     # 轮播区域
     post_bgs = [
         {
@@ -72,7 +98,7 @@ def index():
             'author': 'Ming1',
             'title': 'titleA',
             'imag': '../static/images/home/01.jpg',
-            'href_author': '#',
+            'href_author': "http://127.0.0.1:5000/author/sadasd#four",  # 实现跳转
             'href_blog': '#'
         },
         {
@@ -250,20 +276,62 @@ def blog(blog_id):
         return render_template('single-audio.html', blog=blog, comment_number=comment_number, comments=comments)
 
 
-@app.route('/upload',methods=['POST','GET'])  # 文章提交页面
+@app.route('/upload', methods=['POST', 'GET'])  # 文章提交页面
 def user_upload():
+
+    # 验证session
+    try:
+        uid = session['uid']
+    except:
+        flash('请先登陆')
+        return render_template('login.html', flash=flash)
+
     if request.method == 'POST':
-        # title = request.form.get('title')
-        # # text = request.form.get('text')
+
+        # 必要信息获取
+        title = request.form.get('title')
+        text = request.form.get('text')
         imag = request.files['imag']
-        audio = request.files['audio']
-        # mp3 = request.files('mp3')
+        upload_time = datetime.now()
+        blog_type = 1
+
+        # 上传基本路径
         basepath = os.path.dirname(__file__)
-        upload_path_imag = os.path.join(basepath,'static/upload/images',secure_filename(imag.filename))
-        upload_path_mp3 = os.path.join(basepath,'static/upload/audio',secure_filename(imag.filename))
+
+        # 上传图片
+        upload_path_imag = os.path.join(
+            basepath, 'static/upload/images', secure_filename(imag.filename))  # 文件存入服务器
         imag.save(upload_path_imag)
-        audio.save(upload_path_mp3)
-        return '上传成功'
+        upload_path_imag = os.path.join(
+            'upload/audio', secure_filename(imag.filename))  # 路径存入数据库
+
+        # 是否上传音乐
+        try:
+            audio = request.files['audio']
+            upload_path_mp3 = os.path.join(
+                basepath, 'static/upload/audio', secure_filename(audio.filename))
+            audio.save(upload_path_mp3)
+            upload_path_mp3 = os.path.join(
+                'upload/audio', secure_filename(audio.filename))
+            blog_type = 2
+        except:
+            upload_path_mp3 = None
+
+        blog = Blog_info(
+            title=title,
+            text=text,
+            imag=upload_path_imag,
+            audio=upload_path_mp3,
+            blog_type=blog_type,
+            upload_time=upload_time,
+            user_id=uid  # 外键指向uid
+        )
+
+        db.session.add(blog)
+        db.session.commit()
+
+        return redirect('index')
+
     else:
         return render_template('upload.html')
 
@@ -273,10 +341,21 @@ def user_upload():
 '''
 
 
-@app.route('/author/<authorname>')  # 作者详情页面
-def author():
-    return 1
+@app.route('/author/<authoruid>')  # 作者信息展示页面
+def author(authoruid):
 
-@app.route('/example')
-def example():
-    return render_template('upload.html')
+    # 验证登陆
+    # 验证当前用户是否匹配以显示change
+    change = None
+    try:
+        uid = session['uid']
+    except:
+        flash('请先登陆')
+        return render_template('login.html')
+    if uid == authoruid:
+        change = 1
+        
+    u_data = User_info.query.filter_by(uid=authoruid).first()
+    blogs = Blog_info.query.filter_by(user_id=authoruid).all()
+    # 验证u_data
+    return render_template('author_info.html', author=u_data, blogs=blogs,change=change) #前端检验blogs
